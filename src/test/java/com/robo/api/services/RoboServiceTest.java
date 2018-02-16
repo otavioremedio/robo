4b19b19e5bd6f0895a32a15b05227228a0d3537a
otavioremedio <otavioremedio@gmail.com>
2018-02-15 22:22:49 -0200

    finalizado

--- src/test/java/com/robo/api/services/RoboServiceTest.java
+++ src/test/java/com/robo/api/services/RoboServiceTest.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import com.robo.api.dtos.MovimentoDto;
 import com.robo.api.entities.Movimento;
 
 
